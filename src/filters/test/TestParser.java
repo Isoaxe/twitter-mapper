3ce65582d41635427773fc4fe76bc58b17ae6700
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-19 13:54:53 +0700

    TestParser: Write testLogicOps method
    This method tests the different combinations of AND, OR and NOT operators that can arise. Checks the positioning of parenthesis after parsing to ensure operators work as expected.
    => This concludes Task 3.

--- src/filters/test/TestParser.java
+++ src/filters/test/TestParser.java
@@ -18,6 +18,28 @@ public class TestParser {
     }
 
     @Test
+    // Test combinations of logic operators, e.g. AND, OR and NOT.
+    public void testLogicOps() throws SyntaxError {
+        Filter f1 = new Parser("trump and not purple").parse(); // AND statement
+        assertTrue(f1.toString().equals("(trump and not purple)")); // outer parens only
+
+        Filter f2 = new Parser("trump and not purple or green").parse(); // AND/OR statement
+        assertTrue(f2.toString().equals("((trump and not purple) or green)")); // parens wrap AND terms first
+
+        Filter f3 = new Parser("trump or not purple and green").parse(); // OR/AND statement
+        assertTrue(f3.toString().equals("(trump or (not purple and green))")); // parens wrap AND terms first
+
+        Filter f4 = new Parser("trump and not (purple or green)").parse(); // AND/OR with OR terms pre-wrapped
+        assertTrue(f4.toString().equals("(trump and not (purple or green))")); // outer additional parens only
+
+        Filter f5 = new Parser("trump and not purple and green").parse(); // AND/AND statement
+        assertTrue(f5.toString().equals("((trump and not purple) and green)")); // parens wrap first AND terms first
+
+        Filter f6 = new Parser("trump or not not not purple").parse(); // NOT x 3
+        assertTrue(f6.toString().equals("(trump or not not not purple)")); // outer parens only
+    }
+
+    @Test
     public void testHairy() throws SyntaxError {
         Filter x = new Parser("trump and (evil or blue) and red or green and not not purple").parse();
         assertTrue(x.toString().equals("(((trump and (evil or blue)) and red) or (green and not not purple))"));
