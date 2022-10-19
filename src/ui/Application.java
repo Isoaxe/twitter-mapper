96bdff805c656bc97a9e90aa3c04bac1ee8b7a9a
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-19 21:46:38 +0700

    Application: Add observer in addQuery method
    Pass the query param to the addObserver method in twitterSource.
    This registers the observer with the subject.
    Also remove the TODO comment.

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -55,7 +55,7 @@ public class Application extends JFrame {
         Set<String> allterms = getQueryTerms();
         twitterSource.setFilterTerms(allterms);
         contentPanel.addQuery(query);
-        // TODO: This is the place where you should connect the new query to the twitter source
+        twitterSource.addObserver(query);
     }
 
     /**
