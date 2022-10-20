12fd5b775d211398cb6851e4e1c7f3ccf5a3a47e
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-20 22:18:34 +0700

    MapMarkerComplex: Save color as field
    + Create a new field for color.
    + Assign the argument that was passed to the constructor to the field.
    + Remove setting of color in the constructor.
    + Increase marker size.
    + Remove unused import and whitespace.

--- src/ui/MapMarkerComplex.java
+++ src/ui/MapMarkerComplex.java
@@ -5,18 +5,17 @@ import org.openstreetmap.gui.jmapviewer.Layer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class MapMarkerComplex extends MapMarkerCircle {
-    public static final double defaultMarkerSize = 5.0;
+    public static final double defaultMarkerSize = 25.0;
     private Image miniProfilePic;
-    private  static final int profilePicSide = 20;
+    private Color color;
+    private static final int profilePicSide = 20;
 
     public MapMarkerComplex(Layer layer, Coordinate coOrd, Color color, Image miniProfilePic) {
         super(layer, null, coOrd, defaultMarkerSize, STYLE.FIXED, getDefaultStyle());
         this.miniProfilePic = miniProfilePic;
-        setColor(Color.BLACK);
-        setBackColor(color);
+        this.color = color;
     }
 
     @Override
