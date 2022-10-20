447e94dee936f0f7a5d59056e3c37d1e76d36af4
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-20 21:46:29 +0700

    MapMarkerComplex: Override paint method
    The paint method gets run when the MapMarker gets rendered. Draw the users profile pic image where the tweet originated from.
    Note that the circle marker no longer gets displayed, just the pic.

--- src/ui/MapMarkerComplex.java
+++ src/ui/MapMarkerComplex.java
@@ -10,11 +10,19 @@ import java.awt.image.BufferedImage;
 public class MapMarkerComplex extends MapMarkerCircle {
     public static final double defaultMarkerSize = 5.0;
     private Image miniProfilePic;
+    private  static final int profilePicSide = 20;
 
     public MapMarkerComplex(Layer layer, Coordinate coOrd, Color color, Image miniProfilePic) {
         super(layer, null, coOrd, defaultMarkerSize, STYLE.FIXED, getDefaultStyle());
         this.miniProfilePic = miniProfilePic;
         setColor(Color.BLACK);
         setBackColor(color);
+    }
+
+    @Override
+    public void paint(Graphics g, Point position, int radius) {
+        int profilePicX = position.x - profilePicSide / 2;
+        int profilePicY = position.y - profilePicSide / 2;
+        g.drawImage(miniProfilePic, profilePicX, profilePicY, 20, 20, null);
     }
 }
