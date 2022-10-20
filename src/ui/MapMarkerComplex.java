d9654da7468ca482ba9915d2af2985a66e3d06bd
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-20 21:27:10 +0700

    MapMarkerComplex: Save profile pic to field
    Create a new field for the profile picture. Then assign the argument that was passed to the constructor to the field.

--- src/ui/MapMarkerComplex.java
+++ src/ui/MapMarkerComplex.java
@@ -5,12 +5,15 @@ import org.openstreetmap.gui.jmapviewer.Layer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MapMarkerComplex extends MapMarkerCircle {
     public static final double defaultMarkerSize = 5.0;
+    private Image miniProfilePic;
 
-    public MapMarkerComplex(Layer layer, Coordinate coOrd, Color color) {
+    public MapMarkerComplex(Layer layer, Coordinate coOrd, Color color, Image miniProfilePic) {
         super(layer, null, coOrd, defaultMarkerSize, STYLE.FIXED, getDefaultStyle());
+        this.miniProfilePic = miniProfilePic;
         setColor(Color.BLACK);
         setBackColor(color);
     }
