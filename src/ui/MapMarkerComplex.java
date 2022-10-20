8a1e7e9fc95febb1d7a6192a8f86f495703d1bc1
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-20 14:28:46 +0700

    Rename MapMarkerSimple as MapMarkerComplex
    Right-click -> Refactor -> Rename... in IntelliJ IDEA.
    This makes more sense than making a new class that extends MapMarkerCircle and has additional functionality. May as well modify our existing class to suit.

--- src/ui/MapMarkerComplex.java
+++ src/ui/MapMarkerComplex.java
@@ -6,10 +6,10 @@ import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
 
 import java.awt.*;
 
-public class MapMarkerSimple extends MapMarkerCircle {
+public class MapMarkerComplex extends MapMarkerCircle {
     public static final double defaultMarkerSize = 5.0;
 
-    public MapMarkerSimple(Layer layer, Coordinate coOrd, Color color) {
+    public MapMarkerComplex(Layer layer, Coordinate coOrd, Color color) {
         super(layer, null, coOrd, defaultMarkerSize, STYLE.FIXED, getDefaultStyle());
         setColor(Color.BLACK);
         setBackColor(color);
