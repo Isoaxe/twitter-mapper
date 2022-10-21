509470b0737d758be1a6ee9edcecce231a39d7ae
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-21 10:36:37 +0700

    MapMarkerComplex: Place circle marker
    Need to draw and fill a circle as this functionality was lost when the paint method was overridden. Now we have  a circle coloured like the query with the profile pic centred in it.

--- src/ui/MapMarkerComplex.java
+++ src/ui/MapMarkerComplex.java
@@ -5,6 +5,7 @@ import org.openstreetmap.gui.jmapviewer.Layer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class MapMarkerComplex extends MapMarkerCircle {
     public static final double markerRadius = 13.0;
@@ -22,6 +23,15 @@ public class MapMarkerComplex extends MapMarkerCircle {
     public void paint(Graphics g, Point position, int radius) {
         int profilePicX = position.x - profilePicSide / 2;
         int profilePicY = position.y - profilePicSide / 2;
+        int markerX = position.x - (int) markerRadius;
+        int markerY = position.y - (int) markerRadius;
+        float markerDiameterFloat = (float) markerRadius * 2;
+        int markerDiameterInt = (int) markerRadius * 2;
+
+        g.setClip(new Ellipse2D.Float(markerX, markerY, markerDiameterFloat, markerDiameterFloat));
+        g.setColor(color);
+        g.fillOval(markerX, markerY, markerDiameterInt, markerDiameterInt);
+
         g.drawImage(miniProfilePic, profilePicX, profilePicY, 20, 20, null);
     }
 }
