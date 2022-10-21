333553144cf487178ae6308f0926d410c5fcbb63
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-10-21 10:23:17 +0700

    MapMarkerComplex: Update variable name and value
    Specify that it's the radius rather than 'size', which is ambiguous.

--- src/ui/MapMarkerComplex.java
+++ src/ui/MapMarkerComplex.java
@@ -7,13 +7,13 @@ import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
 import java.awt.*;
 
 public class MapMarkerComplex extends MapMarkerCircle {
-    public static final double defaultMarkerSize = 25.0;
+    public static final double markerRadius = 13.0;
     private Image miniProfilePic;
     private Color color;
     private static final int profilePicSide = 20;
 
     public MapMarkerComplex(Layer layer, Coordinate coOrd, Color color, Image miniProfilePic) {
-        super(layer, null, coOrd, defaultMarkerSize, STYLE.FIXED, getDefaultStyle());
+        super(layer, null, coOrd, markerRadius, STYLE.FIXED, getDefaultStyle());
         this.miniProfilePic = miniProfilePic;
         this.color = color;
     }
